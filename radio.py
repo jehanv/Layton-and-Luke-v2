772692d09e90bf9af2f4f19d22a3c5f88782e247
ColtsFan18 <63676869+jbvann05@users.noreply.github.com>
2022-09-14 16:02:58 -0700

    .

--- radio.py
+++ radio.py
@@ -25,10 +25,7 @@ class Radio(Extension):
     async def play_track(self):
         while True:
             audio = AudioVolume("tracks/"+next_song+".mp4")
-            if self.bot.get_bot_voice_state(860674527833620480) is None:
-                await self.start_radio(self.bot.get_channel(1011798163457327156))
-            else:
-                await self.bot.get_bot_voice_state(860674527833620480).play(audio)
+            await self.bot.get_bot_voice_state(860674527833620480).play(audio)
 
     async def start_radio(self, channel: GuildVoice):
         await channel.connect(deafened=True)
